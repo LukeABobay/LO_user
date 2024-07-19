d1b880222bb22f5acc20202fc303fc39eac3d8c2
LukeABobay <81708541+LukeABobay@users.noreply.github.com>
2024-07-19 09:37:31 -0700

    Try changing environment name again

--- get_lo_info.py
+++ get_lo_info.py
@@ -71,7 +71,7 @@ elif (str(HOME) == '/home/parker') & ('perigee' in HOSTNAME):
     roms_out4 = Path('/data2/parker/LiveOcean_roms/output')
 
 elif (str(HOME) == '/home/bobayl') & ('apogee' in HOSTNAME):
-    lo_env = 'pm_apogee'
+    lo_env = 'lb_apogee'
     roms_out1 = Path('/dat1/parker/LO_roms')
     roms_out2 = Path('/pgdat2/parker/LO_roms')
 
