b526cd7ea289702fc322734e371ca345c3d40be6
LukeABobay <81708541+LukeABobay@users.noreply.github.com>
2025-06-15 08:42:01 -0700

    Update run_extractions_and_stage.py

--- extract/box/run_extractions_and_stage.py
+++ extract/box/run_extractions_and_stage.py
@@ -5,8 +5,8 @@ from datetime import datetime, timedelta
 from pathlib import Path
 
 # Configuration
-start_date = datetime(2024, 1, 1)
-end_date = datetime(2024, 6, 30)
+start_date = datetime(2024, 7, 1)
+end_date = datetime(2024, 12, 31)
 gtx = "cas7_t0_x4b"
 roms_out_num = "1"
 job = "bobay"
